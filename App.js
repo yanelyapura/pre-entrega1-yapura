d2bef2c5500f52d488cc5564ad26e3a0157cf5b9
yanelyapura <yanelyapura@gmail.com>
2024-01-24 20:18:30 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
 
-export default function App() {
+const App = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <View style={styles.container}>
+    <Text style={styles.text}>Bienvenidos</Text>
+  </View>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App 
+
+const styles= StyleSheet.create({
+container:{
+  backgroundColor: "#000",
+  flex:1
+},
+text:{
+  color:"white"
+}
+
+})
